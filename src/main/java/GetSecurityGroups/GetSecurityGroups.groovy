94934784afe1305dd5975b230a51b23b7b1a810c
kesigan <kesigan@gmail.com>
2020-10-03 17:55:53 +0530

    Updated for each

--- src/main/java/GetSecurityGroups/GetSecurityGroups.groovy
+++ src/main/java/GetSecurityGroups/GetSecurityGroups.groovy
@@ -12,8 +12,7 @@ class GetSecurityGroups {
         final AmazonS3 s3= AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build()
         List<Bucket> buckets=s3.listBuckets()
         System.out.println("Amazon S3 buckets are:")
-        for(Bucket b: buckets)
-        {
+        buckets.each { Bucket b ->
             System.out.println(b.getName())
         }
     }
