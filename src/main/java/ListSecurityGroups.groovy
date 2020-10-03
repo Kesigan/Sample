fc72f1eaf5ebde8aeb326e373de2871ec315bdce
kesigan <kesigan@gmail.com>
2020-10-03 18:05:00 +0530

    initial commit

--- src/main/java/ListSecurityGroups.groovy
+++ src/main/java/ListSecurityGroups.groovy
@@ -1,13 +1,9 @@
-package GetSecurityGroups
-
-
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.Bucket
 
-class GetSecurityGroups {
-
+class ListSecurityGroups {
     public static void main(String[] args){
         final AmazonS3 s3= AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build()
         List<Bucket> buckets=s3.listBuckets()
@@ -16,4 +12,5 @@ class GetSecurityGroups {
             System.out.println(b.getName())
         }
     }
+
 }
